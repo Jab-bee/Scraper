3dd2611f85659144232f02c056e3d675ff3ef5a9
Jab-be <oneprueba123@gmail.com>
2025-08-27 09:17:23 +0200

    Subida de documentos actualizados

---
+++ scraper/services/scrape.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+def scrape_website():
+    # Configurar Selenium
+    options = Options()
+    options.add_argument('--headless')  # Ejecutar en modo headless
+    options.add_argument('--no-sandbox')  # Requerido para algunos servidores
+    options.add_argument('--disable-dev-shm-usage')  # Para evitar errores de memoria
+
+    # Configurar el servicio de GeckoDriver
+    service = Service("/usr/local/bin/geckodriver")
+    # Crear el WebDriver de Firefox
+    driver = webdriver.Firefox(service=service, options=options)
+
+    # Navegar al sitio web
+    url = "https://jorgebenitezlopez.com"
+    driver.get(url)
+    print(driver.title)  
+# Esperar a que los elementos estén presentes
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "h1"))
+        )
+        titles = driver.find_elements(By.CSS_SELECTOR, "h1")
+        urls = driver.find_elements(By.CSS_SELECTOR, "a")
+    except Exception as e:
+        print("Error al encontrar los elementos:", e)
+        driver.quit()
+        return []
+
+    scraped_data = []
+    for title, link in zip(titles, urls):
+        scraped_data.append({
+            "title": title.text,
+            "url": link.get_attribute("href"),
+        })
+
+    print("Scraped data:", scraped_data)  # Para depuración
+    driver.quit()
+    return scraped_data
